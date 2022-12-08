5fe5b63298813a089abcd24eacf4b704ecb68ae4
lunven <servanelunven@gmail.com>
2022-12-08 14:28:35 +0100

    Create AbstractImplicitSolver.h

---
+++ src/AbstractImplicitSolver.h
@@ -0,0 +1,25 @@
+//
+// Created by anyam on 30.11.2022.
+//
+
+#ifndef ABSTRACTIMPLICITSOLVERHEADERDEF
+#define ABSTRACTIMPLICITSOLVERHEADERDEF
+
+#include "AbstractOdeSolver.hpp"
+
+class AbstractImplicitSolver : public AbstractOdeSolver{
+public:
+    AbstractImplicitSolver();
+    AbstractImplicitSolver(const double h, const double t0, const double t1, const double y0,
+                           double (*f)(double y, double t),double (*df)(double y, double t),const unsigned int s);
+    virtual ~AbstractImplicitSolver();
+
+    void SetdRightHandSide(double (*f)(double y, double t));
+    double dRightHandSide(double y, double t) const;
+
+private:
+    double (*df_rhs)(double y, double t);
+};
+
+
+#endif
