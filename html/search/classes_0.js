2a5f8d0f29906396700f48c32d43242f025e59e0
Anya M <anya-aurore.mauron@epfl.ch>
2022-12-14 11:19:18 +0100

    delete other useless folders

--- html/search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['abstractimplicitsolver_0',['AbstractImplicitSolver',['../class_abstract_implicit_solver.html',1,'']]],
-  ['adamsmoultonsolver_1',['AdamsMoultonSolver',['../class_adams_moulton_solver.html',1,'']]]
-];
